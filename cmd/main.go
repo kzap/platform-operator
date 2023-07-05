d591e190530baabb110e4b805760ebfc7cf8db7b
Andre Marcelo-Tanner <andre@ada.support>
2023-07-05 00:06:48 -0400

    add config package and have it load config file or defaults in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,8 @@ import (
 
 	configv1alpha1 "mydev.org/platform-operator/api/config/v1alpha1"
 	platformv1 "mydev.org/platform-operator/api/platform/v1"
+
+	"mydev.org/platform-operator/internal/config"
 	controller "mydev.org/platform-operator/internal/controller/platform"
 	//+kubebuilder:scaffold:imports
 )
@@ -51,14 +53,11 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
+	var configFile string
+	flag.StringVar(&configFile, "config", "",
+		"The controller will load its initial configuration from this file. "+
+			"Omit this flag to use the default configuration values. ")
+
 	opts := zap.Options{
 		Development: true,
 	}
@@ -66,26 +65,42 @@ func main() {
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	setupLog.Info("Initializing")
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "dcd661b7.mydev.org",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
-	})
+	// load config file
+	options, cfg, err := apply(configFile)
+	if err != nil {
+		setupLog.Error(err, "Unable to load the configuration")
+		os.Exit(1)
+	}
+
+	kubeConfig := ctrl.GetConfigOrDie()
+	kubeConfig.QPS = *cfg.ClientConnection.QPS
+	kubeConfig.Burst = int(*cfg.ClientConnection.Burst)
+	setupLog.V(2).Info("K8S Client", "qps", kubeConfig.QPS, "burst", kubeConfig.Burst)
+
+	/*
+		mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+			Scheme:                 scheme,
+			MetricsBindAddress:     metricsAddr,
+			Port:                   9443,
+			HealthProbeBindAddress: probeAddr,
+			LeaderElection:         enableLeaderElection,
+			LeaderElectionID:       "dcd661b7.mydev.org",
+			// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
+			// when the Manager ends. This requires the binary to immediately end when the
+			// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
+			// speeds up voluntary leader transitions as the new leader don't have to wait
+			// LeaseDuration time first.
+			//
+			// In the default scaffold provided, the program ends immediately after
+			// the manager stops, so would be fine to enable this option. However,
+			// if you are doing or is intended to do any operation such as perform cleanups
+			// after the manager stops then its usage might be unsafe.
+			// LeaderElectionReleaseOnCancel: true,
+		})
+	*/
+	mgr, err := ctrl.NewManager(kubeConfig, options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
@@ -114,4 +129,19 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+func apply(configFile string) (ctrl.Options, configv1alpha1.OperatorConfig, error) {
+	options, cfg, err := config.Load(scheme, configFile)
+	if err != nil {
+		return options, cfg, err
+	}
+
+	cfgStr, err := config.Encode(scheme, &cfg)
+	if err != nil {
+		return options, cfg, err
+	}
+	setupLog.Info("Successfully loaded configuration", "config", cfgStr)
+
+	return options, cfg, nil
 }
